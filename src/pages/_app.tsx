5655d4eafb25fd53fdfc536ae8cc7dc17c90f5ca
omitea8 <omitea8@gmail.com>
2023-07-07 06:43:19 +0900

    Headの削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import type { AppProps } from "next/app";
 import MenuBar from "./components/menuBar";
-import Head from "next/head";
 import { Container, Stack } from "@mui/material";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>omi.t Portfolio</title>
-      </Head>
       <Container maxWidth={"md"} sx={{ paddingBottom: "30px" }}>
         <Stack spacing={3}>
           <MenuBar />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <body>
-        <Head />
         <Main />
         <NextScript />
       </body>
