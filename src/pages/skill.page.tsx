2c40b3ca4175a6c4dff777d0caf7a0a5ac0d0506
omitea8 <omitea8@gmail.com>
2023-10-14 11:54:40 +0900

    数値をざっくり入力

--- src/pages/skill.page.tsx
+++ src/pages/skill.page.tsx
@@ -28,31 +28,31 @@ const SkillPage: React.FC = () => {
   const languageSkill = [
     { name: "CSS", level: 40, icon: <SiCss3 /> },
     { name: "HTML5", level: 60, icon: <SiHtml5 /> },
-    { name: "JavaScript", level: 50, icon: <SiJavascript /> },
+    { name: "JavaScript", level: 70, icon: <SiJavascript /> },
     { name: "TypeScript", level: 80, icon: <SiTypescript /> },
     { name: "Ruby", level: 30, icon: <SiRuby /> },
   ];
   const frameworksAndLibrarySkill = [
-    { name: "React", level: 50, icon: <SiReact /> },
+    { name: "React", level: 80, icon: <SiReact /> },
     { name: "Next.js", level: 50, icon: <SiNextdotjs /> },
     { name: "Ruby on Rails", level: 30, icon: <SiRubyonrails /> },
-    { name: "MUI", level: 50, icon: <SiMui /> },
+    { name: "MUI", level: 80, icon: <SiMui /> },
   ];
   const infrastructureSkill = [
     { name: "AWS(S3)", level: 30, icon: <SiAmazonaws /> },
-    { name: "Vercel", level: 50, icon: <SiVercel /> },
+    { name: "Vercel", level: 30, icon: <SiVercel /> },
     { name: "Heroku", level: 30, icon: <SiHeroku /> },
-    { name: "Render", level: 50, icon: <SiRender /> },
+    { name: "Render", level: 30, icon: <SiRender /> },
   ];
   const otherSkill = [
-    { name: "Git", level: 50, icon: <SiGit /> },
-    { name: "GitHub", level: 50, icon: <SiGithub /> },
-    { name: "PostgreSQL", level: 30, icon: <SiPostgresql /> },
-    { name: "Visual Studio Code", level: 50, icon: <SiVisualstudiocode /> },
-    { name: "Photoshop", level: 50, icon: <SiAdobephotoshop /> },
-    { name: "Illustrator", level: 50, icon: <SiAdobeillustrator /> },
+    { name: "Git", level: 80, icon: <SiGit /> },
+    { name: "GitHub", level: 80, icon: <SiGithub /> },
+    { name: "PostgreSQL", level: 50, icon: <SiPostgresql /> },
+    { name: "Visual Studio Code", level: 70, icon: <SiVisualstudiocode /> },
+    { name: "Photoshop", level: 80, icon: <SiAdobephotoshop /> },
+    { name: "Illustrator", level: 60, icon: <SiAdobeillustrator /> },
     { name: "After Effects", level: 50, icon: <SiAdobeaftereffects /> },
-    { name: "スクラム開発の基礎知識", level: 100, icon: <FaPeopleGroup /> },
+    { name: "スクラム開発の基礎知識", level: 70, icon: <FaPeopleGroup /> },
   ];
 
   return (
