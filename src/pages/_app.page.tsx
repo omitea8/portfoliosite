949c5c5afbe5eee34dcaead1ee1b5a487d60ad11
omitea8 <omitea8@gmail.com>
2023-10-14 11:40:06 +0900

    リンクの下線をnoneに設定

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import MenuBar from "./components/MenuBar";
 import { Container, Stack } from "@mui/material";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
