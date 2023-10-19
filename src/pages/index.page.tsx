6f62ddefb9353afa638cac4141aa741593a234bf
omitea8 <omitea8@gmail.com>
2023-10-19 15:04:28 +0900

    経歴のレイアウト変更

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,7 @@
 import { IconButton, Stack, Typography } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import { TimeLineElement } from "./components/TimeLineElement";
-import { Timeline, timelineItemClasses } from "@mui/lab";
+import { HistoryElement } from "./components/HistoryElement";
 
 export default function Home() {
   return (
@@ -29,38 +28,21 @@ export default function Home() {
           現在はアプリケーションの開発の全体を把握できるよう手広く学んできました。今後はフロントエンドの技術に週中し、より専門的な知識を身につけていきたいと考えています。
         </Typography>
       </Stack>
-      <Stack spacing={1}>
+      <Stack spacing={2}>
         <Typography variant="h4">経歴</Typography>
-        <Timeline
-          sx={{
-            [`& .${timelineItemClasses.root}:before`]: {
-              flex: 0,
-              padding: 0,
-            },
-          }}
-        >
-          <TimeLineElement
-            day="2008"
-            title="私立済美高等学校 美術科(愛媛)"
-            line={true}
-          />
-          <TimeLineElement
-            day="2010"
-            title="日本電子専門学校 アニメーション科"
-            line={true}
-          />
-          <TimeLineElement day="2013" title="飲食店アルバイト" line={true} />
-          <TimeLineElement
-            day="2022"
-            title="派遣社員として一般事務"
-            line={true}
-          />
-          <TimeLineElement
-            day="現在"
-            title="エンジニアになるために勉強中"
-            line={false}
-          />
-        </Timeline>
+        <HistoryElement
+          day="~2008"
+          title="私立済美高等学校 美術科"
+          // description="素描を始め油絵や日本画や彫刻、歴史など幅広く美術について学びました。専攻はデザインで、商業デザインについて学びながら作品と向き合うことを体験しました。"
+        />
+        <HistoryElement
+          day="~2010"
+          title="日本電子専門学校 アニメーション科"
+          // description="アニメ作成の一通りを学びました。背景の作画とAEを使った撮影などを主に担当し、大人数でのチーム制作を体験しました。"
+        />
+        <HistoryElement day="~2013" title="飲食店アルバイト" />
+        <HistoryElement day="~2022" title="派遣社員として一般事務" />
+        <HistoryElement day="~現在" title="エンジニアになるために勉強中" />
       </Stack>
     </Stack>
   );
