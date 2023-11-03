4c5ec4f5a53e195a1bfc5e40bba910fe3c40270d
omitea8 <omitea8@gmail.com>
2023-11-03 23:48:52 +0900

    勉強履歴を新しい順に変更

--- src/pages/study.page.tsx
+++ src/pages/study.page.tsx
@@ -17,37 +17,33 @@ const studyPage = () => {
             },
           }}
         >
-          {/* 【JavaScript&CSS】ガチで学びたい人のためのWEB開発徹底実践（フロントエンド編） */}
+          {/* フロントエンドのテスト */}
           <StudyElement
-            url="https://www.notion.so/JavaScript-CSS-WEB-0e63a6dcb35c484895c30494ed0dcb85?pvs=4"
-            day="2022/04-"
-            title="Udemyの動画でHTML5とCSS3の基礎を学ぶ"
-            description="
-              HTMLは学生のころ少しだけ触ったことがあったため、比較的入りやすいと思いまずはHTMLとCSSの基礎を学びました。"
+            url="https://www.notion.so/c23a9d77e3e74ba3bdf83a990753171b?pvs=4"
+            day="2023/10-"
+            title="フロントエンド開発のテストについて学ぶ"
+            description="フロントエンドのテストについての手法や使い分けなど具体的なことについて知識不足を感じたため学習を始めました。"
           />
-          {/* りあクト！ */}
+          {/* paiza */}
           <StudyElement
-            url="https://www.notion.so/70a624b08c8e4039b45ecdb24e749f39?pvs=4"
-            day="2022/06-"
-            title="書籍りあクト!でJavaScript,TypeScript,Reactの基礎を学ぶ"
-            description="
-              近年のフロントエンドはReactやNext.jsなどが多く使われていると知り汎用性を考え、今回はReactを選択しました。また、JavaScriptの基礎は他の言語でも応用できると思い、ひとまずはReactで使う技術のみに集中しました。"
+            url="https://www.notion.so/paiza-eacece5acd324c2fa81f177120371816?pvs=4"
+            day="2023/04-"
+            title="paiza・競プロでアルゴリズムや実力を伸ばす"
+            description="技術力や実装力が足りないと感じたため、競プロを始めました。"
           />
-          {/* わかりみSQL */}
+          {/* caita */}
           <StudyElement
-            url="https://www.notion.so/SQL-aa0b4ce458834185befd4c25eb1d0ac8?pvs=4"
-            day="2022/09-"
-            title="書籍わかりみSQLでSQL(PostgreSQL)を学ぶ"
-            description="
-              実際にアプリケーションを作成するときに関連する技術全体の知識があった方が良いと考えたため、データベースについて基礎的なことを学びました。"
+            url="https://www.notion.so/caita-1ccf1ed783a543fc98e7a6057fdc78f0?pvs=4"
+            day="2023/01-"
+            title="自主開発(画像投稿Webアプリケーション)を開始"
+            description="学んだ知識を活かしてさらに実践的なものを作ろうと考えていたためこの開発に至りました。また、このアプリケーションが自身のエンジニアリングの動機でもあります。"
           />
-          {/* WEB技術がこれ1冊でしっかりわかる教科書 */}
+          {/* スクラムガイド */}
           <StudyElement
-            url="https://www.notion.so/WEB-1-8c46853fb6974873a8bca5debfefac91?pvs=4"
-            day="2022/10-"
-            title="書籍WEB技術がこれ1冊でしっかりわかる教科書でWebの仕組みについて学ぶ"
+            day="2022/12-"
+            title="スクラムガイド2020でスクラム開発の基礎について学ぶ"
             description="
-              Webの仕組みや用語について体系的に学んだ経験がなかったため、どうやってWebが成り立っているかなど基礎的なことを学びました。"
+              認定スクラムマスターの資格保有者とスクラムガイドの読み合わせを通してスクラムの基礎について学びました。開発手法としてアジャイル開発やスクラムが多く使われていると知り学んでおいた方が良いと感じたためこの読み合わせを開催してもらいました。また、自主開発においてもスクラム開発で行っています。"
           />
           {/* Railsチュートリアル */}
           <StudyElement
@@ -59,33 +55,37 @@ const studyPage = () => {
               on
               Railsでのアプリケーション開発も活発と知り、このチュートリアルを選択しました。"
           />
-          {/* スクラムガイド */}
+          {/* WEB技術がこれ1冊でしっかりわかる教科書 */}
           <StudyElement
-            day="2022/12-"
-            title="スクラムガイド2020でスクラム開発の基礎について学ぶ"
+            url="https://www.notion.so/WEB-1-8c46853fb6974873a8bca5debfefac91?pvs=4"
+            day="2022/10-"
+            title="書籍WEB技術がこれ1冊でしっかりわかる教科書でWebの仕組みについて学ぶ"
             description="
-              認定スクラムマスターの資格保有者とスクラムガイドの読み合わせを通してスクラムの基礎について学びました。開発手法としてアジャイル開発やスクラムが多く使われていると知り学んでおいた方が良いと感じたためこの読み合わせを開催してもらいました。また、自主開発においてもスクラム開発で行っています。"
+              Webの仕組みや用語について体系的に学んだ経験がなかったため、どうやってWebが成り立っているかなど基礎的なことを学びました。"
           />
-          {/* caita */}
+          {/* わかりみSQL */}
           <StudyElement
-            url="https://www.notion.so/caita-1ccf1ed783a543fc98e7a6057fdc78f0?pvs=4"
-            day="2023/01-"
-            title="自主開発(画像投稿Webアプリケーション)を開始"
-            description="学んだ知識を活かしてさらに実践的なものを作ろうと考えていたためこの開発に至りました。また、このアプリケーションが自身のエンジニアリングの動機でもあります。"
+            url="https://www.notion.so/SQL-aa0b4ce458834185befd4c25eb1d0ac8?pvs=4"
+            day="2022/09-"
+            title="書籍わかりみSQLでSQL(PostgreSQL)を学ぶ"
+            description="
+              実際にアプリケーションを作成するときに関連する技術全体の知識があった方が良いと考えたため、データベースについて基礎的なことを学びました。"
           />
-          {/* paiza */}
+          {/* りあクト！ */}
           <StudyElement
-            url="https://www.notion.so/paiza-eacece5acd324c2fa81f177120371816?pvs=4"
-            day="2023/04-"
-            title="paiza・競プロでアルゴリズムや実力を伸ばす"
-            description="技術力や実装力が足りないと感じたため、競プロを始めました。"
+            url="https://www.notion.so/70a624b08c8e4039b45ecdb24e749f39?pvs=4"
+            day="2022/06-"
+            title="書籍りあクト!でJavaScript,TypeScript,Reactの基礎を学ぶ"
+            description="
+              近年のフロントエンドはReactやNext.jsなどが多く使われていると知り汎用性を考え、今回はReactを選択しました。また、JavaScriptの基礎は他の言語でも応用できると思い、ひとまずはReactで使う技術のみに集中しました。"
           />
-          {/* フロントエンドのテスト */}
+          {/* 【JavaScript&CSS】ガチで学びたい人のためのWEB開発徹底実践（フロントエンド編） */}
           <StudyElement
-            url="https://www.notion.so/c23a9d77e3e74ba3bdf83a990753171b?pvs=4"
-            day="2023/10-"
-            title="フロントエンド開発のテストについて学ぶ"
-            description="フロントエンドのテストについての手法や使い分けなど具体的なことについて知識不足を感じたため学習を始めました。"
+            url="https://www.notion.so/JavaScript-CSS-WEB-0e63a6dcb35c484895c30494ed0dcb85?pvs=4"
+            day="2022/04-"
+            title="Udemyの動画でHTML5とCSS3の基礎を学ぶ"
+            description="
+              HTMLは学生のころ少しだけ触ったことがあったため、比較的入りやすいと思いまずはHTMLとCSSの基礎を学びました。"
           />
         </Timeline>
       </Stack>
