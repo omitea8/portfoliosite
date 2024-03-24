a712cf33ca71f5a3181b97488dbde7d825e8e4fb
omitea8 <omitea8@gmail.com>
2024-03-25 00:32:29 +0900

    テーマの追加

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,17 +1,31 @@
 import type { AppProps } from "next/app";
 import MenuBar from "../components/MenuBar";
-import { Container, Stack } from "@mui/material";
+import { Container, Stack, ThemeProvider, createTheme } from "@mui/material";
 import "../styles/globals.css";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          lineHeight: 2,
+        },
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Container maxWidth={"lg"} sx={{ paddingBottom: "100px" }}>
-        <Stack spacing={3}>
-          <MenuBar />
-          <Component {...pageProps} />
-        </Stack>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth={"md"} sx={{ paddingBottom: "100px" }}>
+          <Stack spacing={3}>
+            <MenuBar />
+            <Component {...pageProps} />
+          </Stack>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
